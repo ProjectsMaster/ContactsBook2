e1429e4c107a6eb5718320081d7b07abc81f654a
Teacher <teacher@ukr.net>
2023-11-02 21:55:13 +0200

    Group.h done

--- ContactsBook/Group.h
+++ ContactsBook/Group.h
@@ -1,5 +1,20 @@
 #pragma once
+#include "Contact.h"
+
 class Group
 {
+	std::string name;
+	Contact* contacts;
+	int size;
+	int count;
+public:
+	Group();
+	Group(std::string name, int size);
+	~Group();
+	std::string getName() const;
+	Contact* getContacts() const;
+	int getSize() const;
+	int getCount() const;
+	void addContact(const Contact& contact);
+	void delContact(std::string name);
 };
-
